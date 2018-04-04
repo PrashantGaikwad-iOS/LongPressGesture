1701d4a61f974076337ba0152a1b39359a23862a
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2018-04-04 16:31:25 +0530

    Initial commit

--- Day 1 - LongPressGesture/ViewController.swift
+++ Day 1 - LongPressGesture/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  @IBOutlet weak var viewLong: UIView!
+  
+  var longGesture = UILongPressGestureRecognizer()
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // LongPress Gesture
+    longGesture = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPress(_:)))
+    longGesture.minimumPressDuration = 1
+    viewLong.addGestureRecognizer(longGesture)
+    
+    
   }
 
+  
+  @objc func longPress(_ sender: UILongPressGestureRecognizer) {
+    let alertC = UIAlertController(title: "Long Press", message: "Long press gesture called when you press on view of 1 second duration.", preferredStyle: UIAlertControllerStyle.alert)
+    let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (alert) in
+    }
+    alertC.addAction(ok)
+    self.present(alertC, animated: true, completion: nil)
+  }
+  
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
